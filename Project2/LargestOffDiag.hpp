0b4e8729637162cbd497aacf790ca22bbdb80512
henrikbreitenstein <henrik.breitenstein@bremo.org>
2021-09-16 13:46:33 +0200

    problem 4 start

---
+++ Project2/LargestOffDiag.hpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <armadillo>
+
+double max_offdiag_symmetric(const arma::mat& A, int& k, int& l){
+  int rows = A.n_rows;
+  int cols = A.n_cols;
+  double current_highest = 0;
+  //Tester kun for den øvre delen av diagonalen siden A er symmetrisk
+  //Vil si at i går gjennom alle kolonner
+  //Og at j går fra den kolonnens diagonalelement og utover
+  std::cout << A;
+  for (int i = 0; i < rows; i++){
+    for (int j = i+1; j < cols; j++){
+      //Matrisene henter element row-col så blir omvendt av for-løkkene
+      double element = abs(A(i,j));
+      std::cout << i << j;
+      if (element > current_highest){
+        current_highest = A(i, j);
+        //Setter rad-nummeret til k og kolonnenummeret til l
+        int& k = i;
+        int& l = j;
+      }
+    }
+  }
+  std::cout << current_highest << " " << k << " "<< l;
+  return current_highest;
+}
---
+++ Project2/LargestOffDiagTest.cpp
@@ -0,0 +1,14 @@
+#include "LargestOffDiag.hpp"
+int main(){
+  arma::mat A(4,4, arma::fill::zeros);
+  arma::vec c = {0.5, -0.7, -0.7, 0.5};
+  int rows = A.n_rows;
+  for (int i=0; i<rows; i++){
+    A(i, i) = 1;
+    A(rows-i -1, i) = c(i);
+  }
+  int k = 0;
+  int l = 0;
+  double MaxAbs = max_offdiag_symmetric(A, k, l);
+  std::cout << MaxAbs << ", k = " << k << ", l = "<< l << std::endl;
+}
