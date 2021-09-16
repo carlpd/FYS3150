1641f08a69d80a3697ab8b2d161e1d0c024a9939
carlpd <carlpd@fys.uio.no>
2021-09-16 13:44:06 +0200

    Porblem 3 ferdig

--- Project2/prob3.cpp
+++ Project2/prob3.cpp
@@ -1,5 +1,8 @@
 #include <armadillo>
 #include <iostream>
+//#include <cstdlib>
+#include <cmath>
+#include <math.h>
 using namespace arma;
 int main(){
   int n=7;
@@ -8,16 +11,30 @@ int main(){
   mat A=mat(N,N).fill(0.);
   double a=-1.0/h;
   double d=2.0/h;
-  for (int i=0;i<N-1; i++){
-    A(i)=a;
-    A(i+1)=d;
-    A(i+2)=a;
+  vec lam=vec(N).fill(0.);
+  mat v=mat(N,N).fill(0.);
+  for (int i=0;i<=N-1; i++){
+    std::cout<<i<<std::endl;
+    lam(i)=d+2*a*cos((i+1)*M_PI/(N+1));
+    for (int j=0;j<=N-1; j++){
+      v(j,i)=sin((j+1)*(i+1)*M_PI*1.0/(1.0*N+1));
+    }
   }
-  A(N-2)=a;
-  A(N-1)=d;
+  A(0,0)=d;
+  A(0,1)=a;
+  for (int i=1;i<N-1; i++){
+    A(i,i-1)=a;
+    A(i,i)=d;
+    A(i,i+1)=a;
+  }
+  A(N-1,N-2)=a;
+  A(N-1,N-1)=d;
+    std::cout<<"A"<<A<<std::endl;
   mat eigvec;
   vec eigval;
   eig_sym(eigval, eigvec, A);
-  std::cout<<eigval<<std::endl;
+  std::cout<<eigval<<"teoretiske"<<lam<<std::endl;
+  std::cout<<normalise(eigvec)<<std::endl;
+  std::cout<<normalise(v)<<std::endl;
   return 0;
 }
