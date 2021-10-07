bb9c9c1189a46cdf281b447eeff23a38e2d806d0
carlpd <carlpd@fys.uio.no>
2021-10-07 13:05:14 +0200

    Nå kjører noe i hvert fall

--- Project3/hppfunk/p3.hpp
+++ Project3/hppfunk/p3.hpp
@@ -1,15 +1,35 @@
 #include <armadillo>
-
+#include <iostream>
+#include <cmath>
 class Particle{
-public:
-  double q; //Charge
-  double m; //Mass
-  arma::vec r; //position
-  arma::vec v; //Velocity
+  public:
+    double q; //Charge
+    double m; //Mass
+    arma::vec r; //position
+    arma::vec v; //Velocity
+    Particle(double q_in, double m_in , arma::vec r_in, arma::vec v_in);
 };
-class PenningTrap{
-public:
-  double B0;
-  double V0;
-  double d;
-}
+class PenningTrap
+{
+  public:
+    double B0;
+    double V0;
+    double d;
+    std::vector<class Particle> parts;
+    PenningTrap(double B_in, double V_in, double d_in, std::vector<class Particle> parts_in);
+    void add_particle(Particle vals);
+    void add_randomparticle();
+    arma::vec FindE(arma::vec r);
+    arma::vec FindB(double B0);
+    arma::vec EksF(Particle j);
+    arma::vec pij(Particle i, Particle j);
+    arma::vec PF(int j);
+    arma::vec TF(int j);
+    void evolve_Euler(double dt);
+};
+/*
+arma::vec EksF(double V0, double B0, double d, Particle j);
+arma::vec pij(Particle i, Particle j);
+arma::vec PF(Particle parts, int j);
+arma::vec TF(std::vec parts, int j, double B0, double V0, double d);
+*/
---
+++ Project3/main.cpp
@@ -0,0 +1,17 @@
+#include "../hppfunk/p3.hpp"
+double ke=1.38935333e5;
+double Tesla=9.64852558e1;
+double Volt=9.64852558e7;
+int main(){
+
+  Particle first_particle(q,m,r,v)
+  //PenningTrap.add_particle(first_particle)
+
+  tV0=10.0*Volt;
+  tB0=1.0*Tesla;
+  td=1e4;
+  std::vector<class Particle> tparts;
+  PenningTrap test=Penningtrap(tV0, tB0, td, tparts);
+  test.add_randomparticle();
+  return 0;
+}
