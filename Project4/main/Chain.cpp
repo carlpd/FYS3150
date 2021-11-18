c1e93ada4e4680251f432a026c1d1600447fef6b
carlpd <carlpd@fys.uio.no>
2021-11-18 13:27:23 +0100

    Noe, kanskje feil

--- Project4/main/Chain.cpp
+++ Project4/main/Chain.cpp
@@ -2,57 +2,61 @@
 #include <fstream>
 
 int main(){
+  /*
   arma::vec T=arma::vec(2).fill(0.);
   arma::vec L=arma::vec(2).fill(0.);
   T(0)=1.0; T(1)=2.4;
-  L(0)=20; L(1)=40;
+  L(0)=2; L(1)=40;
+  */
   std::ofstream Time ("Txt/Time.txt", std::ios::app);
   std::ofstream Ttxt ("Txt/T.txt", std::ofstream::out);
   std::ofstream Ltxt ("Txt/L.txt", std::ofstream::out);
   auto t1 = std::chrono::high_resolution_clock::now();
+  /*
   for(int Ti=0;Ti<T.n_elem;Ti++){
-    double T_in = T(Ti); /* [J/kb] */
+    double T_in = T(Ti); // [J/kb]
     Ttxt << std::to_string(T_in) << std::endl;
     for(int Li=L.n_elem-1;Li<L.n_elem;Li++){
 
+    */
+    double T_in=1.0;
+    int L_in = 10; /* [-] */
 
-      int L_in = L(Li); /* [-] */
+    Ltxt << std::to_string(L_in) << std::endl;
 
-      Ltxt << std::to_string(L_in) << std::endl;
+    std::string filename;
+    filename="../Txt/ChainT" + std::to_string(T_in) + "L" + std::to_string(L_in) + ".txt";
+    std::ofstream f (filename, std::ofstream::out);
 
-      std::string filename;
-      filename="../Txt/ChainT" + std::to_string(T_in) + "L" + std::to_string(L_in) + ".txt";
-      std::ofstream f (filename, std::ofstream::out);
+    Ising2d IS2D = Ising2d(T_in, L_in);
+    arma::imat dummyS = IS2D.makerandomspins(2413);
+    //std::cout<<IS2D.S_<<std::endl;
+    IS2D.findall();
+    /*
+    double deps = IS2D.ep/IS2D.N; double deps2 = IS2D.ep2/(IS2D.N*IS2D.N);
+    double m = IS2D.M/IS2D.N; double m2 = IS2D.M2/IS2D.N;
+    // Step | eps | eps^2 | m | m^2 | Cv | X
+    f << 1 << " " << deps << " " << deps2 << " ";
+    f << m << " " << m2 << " " << IS2D.Cv << " " << IS2D.X << std::endl;
+    */
 
-      Ising2d IS2D = Ising2d(T_in, L_in);
-      arma::imat dummyS = IS2D.makerandomspins(2413);
+    for(int a=0; a<=IS2D.N_*10; a++){
+      // Step | eps | eps^2 | m | m^2
+      IS2D.makebreakstate();
       //std::cout<<IS2D.S_<<std::endl;
-      IS2D.findall();
-      /*
-      double deps = IS2D.ep/IS2D.N; double deps2 = IS2D.ep2/(IS2D.N*IS2D.N);
-      double m = IS2D.M/IS2D.N; double m2 = IS2D.M2/IS2D.N;
-      // Step | eps | eps^2 | m | m^2 | Cv | X
-      f << 1 << " " << deps << " " << deps2 << " ";
-      f << m << " " << m2 << " " << IS2D.Cv << " " << IS2D.X << std::endl;
-      */
-
-      for(int a=0; a<=IS2D.N_*1000; a++){
-        // Step | eps | eps^2 | m | m^2
-        IS2D.makebreakstate();
-        //std::cout<<IS2D.S_<<std::endl;
-      }
-      for(int a=IS2D.N_*1000; a<=IS2D.N_*10000; a++){
-        // Step | eps | eps^2 | m | m^2
-        f << a << " " << IS2D.ep_ << " " << IS2D.ep2_ << " ";
-        f << IS2D.M_ << " " << IS2D.M2_ <<std::endl;
-        IS2D.makebreakstate();
-        //std::cout<<IS2D.S_<<std::endl;
-      }
-      f.close();
     }
-    Ttxt.close();
-    Ltxt.close();
-  }
+    for(int a=IS2D.N_*1000; a<=IS2D.N_*2000; a++){
+      // Step | eps | eps^2 | m | m^2
+      f << a << " " << IS2D.ep_ << " " << IS2D.ep2_ << " ";
+      f << IS2D.M_ << " " << IS2D.M2_ <<std::endl;
+      IS2D.makebreakstate();
+      //std::cout<<IS2D.S_<<std::endl;
+    }
+    f.close();
+  //}
+  Ttxt.close();
+  Ltxt.close();
+  //}
   auto t2 = std::chrono::high_resolution_clock::now();
   double duration_seconds = std::chrono::duration<double>(t2 - t1).count();
   Time << duration_seconds << std::endl;
--- Project4/main/ChainLong.cpp
+++ Project4/main/ChainLong.cpp
@@ -7,7 +7,7 @@
 int main(){
   //arma::vec T=arma::vec(2).fill(0.);
   //arma::vec L=arma::vec(2).fill(0.);
-  int TN=100;
+  int TN=12;
   double Tstart=2.1;
   double Tend=2.4;
   int LN=4;
@@ -15,7 +15,7 @@ int main(){
   int Lend=100;
   arma::vec T=arma::linspace(Tstart, Tend, TN);
   arma::vec L=arma::vec(LN).fill(0.);
-  L(0)=40; L(1)=60; L(2)=80; L(3)=100;
+  L(0)=10; L(1)=60; L(2)=80; L(3)=100;
   std::ofstream TimePar ("Txt/TimePar.txt", std::ios::app);
   std::ofstream Ttxt ("Txt/T.txt", std::ofstream::out);
   std::ofstream Ltxt ("Txt/L.txt", std::ofstream::out);
@@ -23,9 +23,10 @@ int main(){
   #pragma omp parallel for
   for(int Ti=0;Ti<T.n_elem;Ti++){
     double T_in = T(Ti); /* [J/kb] */
+    std::cout<<T_in<<std::endl;
     Ttxt << std::to_string(T_in) << std::endl;
     int omp_get_thread_num();
-    int L_in = L(2); /* [-] */
+    int L_in = L(0); /* [-] */
 
     Ltxt << std::to_string(L_in) << std::endl;
 
@@ -47,17 +48,17 @@ int main(){
     f << m << " " << m2 << " " << IS2D.Cv << " " << IS2D.X << std::endl;
     */
 
-    for(int a=0; a<=N*1000; a++){
+    for(int a=0; a<=IS2D.N_*1000; a++){
       // Step | eps | eps^2 | m | m^2
       IS2D.makebreakstate();
-      //std::cout<<IS2D.S_<<std::endl;
+      //std::cout<<a<<std::endl;
     }
-    for(int a=IS2D.N_*1000; a<=IS2D.N_*500000; a++){
+    for(int a=IS2D.N_*1000; a<=IS2D.N_*100000; a++){
       // Step | eps | eps^2 | m | m^2
       f << a << " " << IS2D.ep_ << " " << IS2D.ep2_ << " ";
       f << IS2D.M_ << " " << IS2D.M2_ <<std::endl;
       IS2D.makebreakstate();
-      //std::cout<<IS2D.S_<<std::endl;
+      //std::cout<<a<<std::endl;
     }
     f.close();
 
