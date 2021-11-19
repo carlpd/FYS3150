83f1542b49a99327bd9cd9d349f8cea6fcc14d7d
henrikbreitenstein <henrik.breitenstein@bremo.org>
2021-11-19 20:10:23 +0100

    feil ved eps

--- Project4/main/Chain.cpp
+++ Project4/main/Chain.cpp
@@ -3,12 +3,10 @@
 #include <omp.h>
 
 int main(){
-    double T_in=2.4;
-    int L_in = 20; /* [-] */
-
+    double T_in=1.;
     int L_in = 20; /* [-] */
     std::string filename;
-    filename="Txt/ChainT" + std::to_string(T_in) + "L" + std::to_string(L_in) + ".txt";
+    filename="Txt/ChainT" + std::to_string(T_in) + "L" + std::to_string(L_in) + "up.txt";
     std::ofstream f (filename, std::ofstream::out);
 
     Ising2d IS2D = Ising2d(T_in, L_in);
@@ -16,7 +14,7 @@ int main(){
     IS2D.findall();
 
 
-    for(int a=0; a<=IS2D.N_*10000; a++){
+    for(int a=0; a<=IS2D.N_*2000; a++){
       f << a << " " << IS2D.ep_ << " " << IS2D.ep2_ << " ";
       f << IS2D.M_ << " " << IS2D.M2_ <<std::endl;
       IS2D.makebreakstate();
