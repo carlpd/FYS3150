bbb827f8f3263702ed045737012710adedfc48a4
henrikbreitenstein <henrik.breitenstein@bremo.org>
2021-11-21 16:22:58 +0100

    nye ChainLong

--- Project4/main/ChainLong.cpp
+++ Project4/main/ChainLong.cpp
@@ -12,7 +12,7 @@ int main(){
   int Lstart=40;
   int Lend=100;
   std::string filename;
-  filename="Txt/PerTemp80.txt";
+  filename="Txt/PerTemp60.txt";
   std::ofstream f (filename, std::ofstream::out);
   arma::vec T=arma::linspace(Tstart, Tend, TN);
   #pragma omp parallel for
@@ -20,7 +20,7 @@ int main(){
     double T_in = T(Ti); /* [J/kb] */
     std::cout<<T_in<<std::endl;
     int omp_get_thread_num();
-    int L_in = 80; /* [-] */
+    int L_in = 60; /* [-] */
 
     Ising2d IS2D = Ising2d(T_in, L_in);
     int NumThread = omp_get_thread_num();
@@ -28,14 +28,15 @@ int main(){
     arma::imat dummyS = IS2D.makerandomspins(DivRand);
 
     IS2D.findall();
-    long double samplestart=1000*1.0*IS2D.N_;
+    int samplestart=1000*1.0*IS2D.N_;
+    int samplen=500000*1.0*IS2D.N_;
     //std::cout<<"N"<<IS2D.N_<<std::endl;
-    long double samplen=IS2D.N_*1.0*500000;
     std::cout<<"N"<<IS2D.N_<<std::endl;
     std::cout<<"end"<<samplen<<std::endl;
     for(int a=0; a<=samplestart; a++){
       IS2D.makebreakstate();
     }
+    std::cout<<"out"<<std::endl;
     double TE=0;
     double TE2=0;
     double TM=0;
