4d33c2ca8df18fd7c10786fcd333acffeb957d33
carlpd <carlpd@fys.uio.no>
2021-11-26 14:46:05 +0100

    Gjort 2 og 3 og er på 4

--- Project5/main/FindJMakeABRun.cpp
+++ Project5/main/FindJMakeABRun.cpp
@@ -1,12 +1,16 @@
-#include "../hpp/FindJMakeAB.hpp"
-#include <iostream>
+#include "../hpp/AllFunks.hpp"
+
 int main(){
-  arma::cx_mat A=arma::cx_mat(9,9).fill(0.);
-  arma::cx_mat B=arma::cx_mat(9,9).fill(0.);
+  arma::sp_cx_mat A=arma::sp_cx_mat(9,9);
+  arma::sp_cx_mat B=arma::sp_cx_mat(9,9);
   arma::cx_vec a=arma::cx_vec(9).fill(4.);
   arma::cx_vec b=arma::cx_vec(9).fill(-4.);
   arma::cx_double r=(-1,1);
-  MakeABC(A, B, a, b, r);
+  MakeAB(A, B, a, b, r);
   std::cout<<A<<std::endl;
+  arma::cx_vec u=arma::cx_vec(9).fill(5.);
+  arma::cx_vec bn=findb(B,u);
+  arma::cx_vec un=(A,bn);
+  std::cout<<un<<std::endl;
   return 0;
 }
