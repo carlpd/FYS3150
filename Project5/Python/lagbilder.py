30fc8b36c443ee47ecf24cf2022c8871af54cfa5
carlpd <carlpd@fys.uio.no>
2021-12-09 09:43:53 +0100

    Litt endringer i pyfilen

--- Project5/Python/lagbilder.py
+++ Project5/Python/lagbilder.py
@@ -8,7 +8,7 @@ file=open("../Txt/U1.bin", "rb")
 Ubs=arma.cube()
 Ubs=arma.abs(U)
 s=Ubs.n_slices
-print(Ubs.slice(1))
+#print(Ubs.slice(1))
 print(s)
 print(arma.size(Ubs))
 
@@ -16,9 +16,11 @@ dt = 2.5e-5
 tt = [0, 1e-3, 2e-3]
 for t in tt:
     k = int(t/dt)
-    U = Ubs[:][:][k]
-    x = np.arange(0, len(U[0]))
-    y = np.arange(0, len(U[1]))
+    U[:,:] = Ubs[:,:,k]
+    print("U", U)
+    print("k", k)
+    x = np.linspace(0, 1, U.n_cols-1)
+    y = np.arange(0, 1, U.n_rows-1)
     X, Y = np.meshgrid(x, y)
     fig = plt.figure(figsize=(10,8))
     plt.imshow(U)
